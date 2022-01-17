297b6c58f1c3a2c08db541e2369e3930e77ae771
Andreea1696 <andreeapetrescu16@gmail.com>
2022-01-17 14:24:10 +0100

    Create class Film with attributes

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-oop-1</title>
+</head>
+<body>
+    <?php
+        /** GOAL:
+          * DEFINIRE CLASSE Film:
+          *      Attributi:
+          *      - titolo
+          *      - sottotiolo
+          *      - regista
+          * 
+          *      Metodi:
+          *      - costruttore che accetta titolo
+          *      - getFullTitle: 
+          *          Se sottotitolo presente:
+          *            restituisce "titolo: sottotitolo" 
+          *          Se sottotiolo assente:
+          *            restituisce "titolo" 
+          *      - __toString: 
+          *          Se regista presente:
+          *            restituisce "fullTitle | regista"
+          *          Se regista assente:
+          *            restituisce "fullTitle | ???"
+          * 
+          * UTILIZZO OGGETTI Film:
+          * 
+          *      Generare 3 istanze della classe Film:
+          *      - uno con solo il titolo
+          *      - uno con titolo e sottotitolo
+          *      - uno con titolo, sottotiolo e regista
+          * 
+          *      Stampare tutti gli oggetti tramite toString (implicito)
+          * 
+          *  RISULTATO ATTESO:
+          * 
+          *      Matrix | ???
+          *      Fantozzi 2: il ritorno di fantozzi | ???
+          *      Peter Pan: il ritorno all'isola che non c'e' | Robin Budd
+          */
+    
+    class Film {
+
+        // attributi 
+        public $title;
+        public $subTitle;
+        public $movieDirector;
+        
+    }
+    ?>
+</body>
+</html>
